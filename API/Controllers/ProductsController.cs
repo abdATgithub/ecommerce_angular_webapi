701d99d27eda33645df67c132020aea6aab5de5b
abdATgithub <abhinavdubey08@gmail.com>
2020-07-19 20:42:56 +0530

    added repository pattern, new entities, seed data and code refactoring.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -11,17 +12,33 @@ namespace API.Controllers
     [Route("api/[controller]")] 
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repository.GetProductsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var products = await _repository.GetProductBrandsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            var products = await _repository.GetProductTypesAsync();
 
             return Ok(products);
         }
@@ -29,7 +46,7 @@ namespace API.Controllers
         [HttpGet("{productId}")]
         public async Task<ActionResult<Product>> GetProduct(int productId)
         {
-            var prod = await _context.Products.FirstOrDefaultAsync(x => x.Id == productId);
+            var prod = await _repository.GetProductByIdAsync(productId);
             return prod;
         }
     }
