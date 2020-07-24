c2565603e2b754208aff2a89808bb0f874e94230
abdATgithub <abhinavdubey08@gmail.com>
2020-07-25 01:22:32 +0530

    Added CORS support, (pagination, sorting, search using specification pattern) and etc..

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -11,9 +11,26 @@ namespace Infrastructure.Data
         {
             var query = inputQuery;
 
-            if (spec.Criteria!=null)
+            /* Sequence is to be considered as paging can't happen before sorting */
+
+            if (spec.Criteria != null)
             {
-                query=query.Where(spec.Criteria);
+                query = query.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
