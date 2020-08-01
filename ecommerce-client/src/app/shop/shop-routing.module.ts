8efe07c05710cffd75151473a04ab85b7fe4823e
abdATgithub <abhinavdubey08@gmail.com>
2020-08-01 19:04:14 +0530

    Added Http Interceptors for handling the error response from API, added Toastr for displaying the notifications, added routing for errors and not-found pages & code refactor, etc..

--- ecommerce-client/src/app/shop/shop-routing.module.ts
+++ ecommerce-client/src/app/shop/shop-routing.module.ts
@@ -2,7 +2,6 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { ShopComponent } from './shop.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   {path: '', component: ShopComponent},
